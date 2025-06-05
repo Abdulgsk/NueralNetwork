26c138dcd4bda541ce3511a1f53f62cb8088e329
sushvin.m <sushvin.m@research.iiit.ac.in>
2025-06-05 20:19:17 +0530

    cors error 2

--- frontend/src/components/MovieAnalyzer.jsx
+++ frontend/src/components/MovieAnalyzer.jsx
@@ -101,6 +101,7 @@ function MovieAnalyzer() {
       {
         method: "POST",
         mode: 'cors',
+        credentials: 'include', 
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: textToProcess }),
       }
